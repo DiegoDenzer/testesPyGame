24380a63f68fc5c8ab53a87f8fbdda9583674d23
diegodenzer <mel99mel>
2017-10-25 14:19:06 -0200

    constantes e eventos

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import pygame as pg
-
+import constantes as c
 
 def inicio():
     pg.init()
     pg.display.set_caption("teste pygame")
     tela = pg.display.set_mode([300, 300])
     sair = False
-    cor = (255, 255, 255)
-    azul = (0, 0, 255)
-    verde = (0, 255, 0)
+    ret = pg.Rect(10,10, 45, 45)
     relogio = pg.time.Clock()
     sup = pg.Surface((200, 200))
-    sup.fill(azul)
-
+    sup.fill(c.AZUL)
     sup2 = pg.Surface((100, 100))
-    sup2.fill(verde)
+    sup2.fill(c.VERDE)
     while sair != True:
+
         for event in pg.event.get():
+
             if event.type == pg.QUIT:
                 sair = True
+
+            if event.type == pg.MOUSEBUTTONDOWN:
+                ret = ret.move(100, 100)
+
+            if event.type == pg.MOUSEMOTION:
+                ret = ret.move(-10, -10)
+
         relogio.tick(30)
-        tela.fill(cor)
-        tela.blit(sup, [50, 50])
+        tela.fill(c.FUNDO_BRANCO)
+       # tela.blit(sup, [50, 50])
         tela.blit(sup2, [100, 100])
+        pg.draw.rect(tela, c.VERMELHO, ret)
         pg.display.update()
 
 
