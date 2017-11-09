49ffeca56f16ee2e8be7db1fd84310277f89df15
diegodenzer <mel99mel>
2017-11-09 13:30:33 -0200

    teste com texto

--- main.py
+++ main.py
@@ -6,13 +6,24 @@ import constantes as c
 def inicio():
     pg.init()
     pg.display.set_caption("teste pygame")
-    tela = pg.display.set_mode([300, 300])
+    tela = pg.display.set_mode([600, 200])
     sair = False
-    ret = pg.Rect(10,10, 30, 30)
+    ret = pg.Rect(10, 10, 10, 10)
 
-    #                posx , posy, largura, altura
-    barra1 = pg.Rect(100, 250, randrange(0, 100), randrange(0, 150))
-    barra2 = pg.Rect(200, 200, randrange(0, 100), randrange(0, 150))
+
+    #  posx , posy, largura, altura
+    y=0
+    x = 50
+
+    listaBarra = []
+
+    for item in range(0, 11):
+        item = pg.Rect(x, 0, 10, 20)
+        listaBarra.append(item)
+                   
+        item = pg.Rect(x, x, 10, 200 - x)
+        listaBarra.append(item)
+        x = x + 50
 
     relogio = pg.time.Clock()
 
@@ -24,8 +35,6 @@ def inicio():
 
     sup = pg.Surface((300, 30))
     sup.fill(c.VERDE)
-    #sup2 = pg.Surface((100, 100))
-    #sup2.fill(c.VERDE)
 
     while sair != True:
 
@@ -36,19 +45,8 @@ def inicio():
 
             if event.type == pg.MOUSEBUTTONDOWN:
                 pg.mouse.set_pos(150,150)
+                ret = ret.inflate(20,20)
 
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_RIGHT or event.key == pg.K_d:
-                    ret.move_ip(5,0)
-
-                if event.key == pg.K_LEFT or event.key == pg.K_a:
-                    ret.move_ip(-5,0)
-
-                if event.key == pg.K_DOWN or event.key == pg.K_s:
-                    ret.move_ip(0, 5)
-
-                if event.key == pg.K_UP or event.key == pg.K_w:
-                    ret.move_ip(0, -5)
 
         (xant, yant) = (ret.left, ret.top)
         (ret.left, ret.top) = pg.mouse.get_pos()
@@ -56,28 +54,32 @@ def inicio():
         ret.top -= ret.height/2
 
 
+        for item in listaBarra:
 
-        if ret.colliderect(barra1):
-            print('barra1')
-            titulo = font_perdeu.render('teste', 1, c.AZUL)
-            tela.blit(titulo, (120, 0))
-            (ret.left, ret.top) = (xant, yant)
 
-        if ret.colliderect(barra2):
-            print('barra2')
-            text = font_perdeu.render('bateu', 1, c.VERMELHO)
-            tela.blit(text, (100, 100))
-            (ret.left, ret.top) = (xant, yant)
+            if ret.colliderect(item):
+
+                (ret.left, ret.top) = (xant, yant)
+
+
+
+
 
         relogio.tick(30)
         tela.fill(c.BRANCO)
-        tela.blit(sup, [0, 0])
+
         titulo = font_perdeu.render('teste', 1, c.AZUL)
-        tela.blit(titulo, (120, 0))
-        sup.blit(titulo, (120,0))
+
+        for item in listaBarra:
+            pg.draw.rect(tela, c.AZUL, item)
+
         pg.draw.rect(tela, c.VERMELHO, ret)
-        pg.draw.rect(tela, c.AZUL, barra1)
-        pg.draw.rect(tela, c.VERDE, barra2)
+
+
+
+
+
+
         pg.display.update()
 
 
